f1a408eb26b9edfa0e235b17eda6fd9a602d72ac
Trishita Tiwari <tt544@g2-login-02.coecis.cornell.edu>
2025-05-05 15:59:35 -0400

    made embed.py a script that's runnable with a config.json file

--- embed.py
+++ embed.py
@@ -1,42 +1,95 @@
 #!/usr/bin/env python3.8
 
-import os
+import os, sys
 import torch
-from typing import Dict
 from process_dset import load_processed_dataset
-from train_det_router import DecoderModel
+from train_det_router import DecoderModel, DecoderConfig
+
+from model_args import ModelArguments
+from data_args import DataTrainingArguments
+
+from transformers import (
+    AutoTokenizer,
+    HfArgumentParser,
+    TrainingArguments
+)
 
 
 
 def embed_tokens(
         embedder_path: str,
-        embedder_params : Dict[str, int],
+        config : DecoderConfig,
         dataset_name : str, 
         dataset_config : str,
-        batch_size: int
+        batch_size: int,
+        seed : int,
         ):
     
-    embedder = DecoderModel(
-        embedder_params["vocab_size"], 
-        embedder_params["embedding_dim"], 
-        embedder_params["num_heads"], 
-        embedder_params["padding_idx"],
-        output_hidden_states=True
-        )
-    embedder.load_state_dict(torch.load(os.path.join(embedder_path, dataset_name, "embedding_model") , weights_only=True))
+    embedder = DecoderModel(config).from_pretrained(
+        os.path.join(
+            embedder_path, 
+            dataset_name, 
+            "embed_model")
+            )
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    embedder.to(device)
     embedder.eval()
-    dataset = load_processed_dataset(dataset_name, dataset_config, split = "train")
+
+    dataset = load_processed_dataset(
+        dataset_name, 
+        dataset_config, 
+        split = "train", 
+        num_samples = 100000, 
+        seed = seed)
+    
     seq_len = len(dataset["input_ids"][0])
 
-    average_embedding = torch.zeros(batch_size, seq_len, embedder_params["embedding_dim"])
+    average_embedding = torch.zeros(batch_size, seq_len, config.embedding_dim)
 
     for start in range(0, len(dataset), batch_size):
         end = min(start + batch_size, len(dataset))
-        batch = torch.tensor(dataset["input_ids"][start: end])
+        batch = torch.tensor(dataset["input_ids"][start: end], device=device)
         average_embedding += embedder(batch)["hidden_states"].sum(0)
 
     average_embedding = average_embedding / len(dataset)
     
-    with open(os.path.join(embedder_path, dataset_name, dataset_config), 'wb') as fdesc:
+    with open(os.path.join(embedder_path, dataset_name, "embed_model", dataset_config), 'wb') as fdesc:
         torch.save(average_embedding, fdesc)
 
+if __name__ == "__main__":
+    
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+
+    tokenizer_kwargs = {
+        "cache_dir": model_args.cache_dir,
+        "use_fast": model_args.use_fast_tokenizer,
+        "revision": model_args.model_revision,
+        "token": model_args.token,
+        "trust_remote_code": model_args.trust_remote_code,
+    }
+    tokenizer = AutoTokenizer.from_pretrained(model_args.tokenizer_name, **tokenizer_kwargs)
+
+    config = config = DecoderConfig(
+            vocab_size = len(tokenizer), 
+            embedding_dim = model_args.hidden_size, 
+            num_heads = 2,
+            padding_idx = tokenizer.pad_token_id
+        )
+
+    embed_tokens(
+        "output",
+        config,
+        model_args.dataset_name,
+        model_args.dataset_config_name,
+        8,
+        training_args.seed
+        )
